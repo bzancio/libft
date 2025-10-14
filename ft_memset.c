d17e62ac8cd1565c20e4d996968ed996e4564dcd
bzancio <bzancio@gmail.com>
2025-10-14 17:31:04 +0200

    Añadidas ft_memset y ft_bzero

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ibuil <ibuil@student.42madrid.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 13:19:59 by ibuil             #+#    #+#             */
-/*   Updated: 2025/10/10 13:46:22 by ibuil            ###   ########.fr       */
+/*   Updated: 2025/10/14 10:41:43 by ibuil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
+	unsigned char	*p;
+	size_t			pos;
+
+	pos = 0;
+	p = (unsigned char)b;
+	while (pos < len)
+	{
+		p[pos] = (unsigned char)c;
+		len++;
+	}
 	return (b);
 }
